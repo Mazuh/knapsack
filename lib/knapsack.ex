00294e152df69bc20b76ef9c53ac32d52c898d87
mazuh <marcell-mz@hotmail.com>
2024-04-03 00:22:13 -0300

    0/1 knapsack

--- lib/knapsack.ex
+++ lib/knapsack.ex
@@ -1,18 +1,56 @@
 defmodule Knapsack do
   @moduledoc """
-  Documentation for `Knapsack`.
+  Knapsack solution.
   """
 
   @doc """
-  Hello world.
+  0/1 Knapsack Problem.
 
-  ## Examples
+  Given N items where each item has some weight and profit associated with it
+  and also given a bag with capacity W, [i.e., the bag can hold at most W weight in it].
 
-      iex> Knapsack.hello()
-      :world
+  The task is to put the items into the bag such that the sum of profits associated with
+  them is the maximum possible.
 
+  And then return the sum of stored profits.
+
+  Items here are indivisible, their entire weight/profit is added or removed from the bag.
   """
-  def hello do
-    :world
+  def zero_one_knapsack(items, weight_limit) do
+    keep_packing(items, weight_limit)
+    |> Enum.map(& &1.profit)
+    |> Enum.sum()
+  end
+
+  defp keep_packing([], _weight_limit) do
+    []
+  end
+
+  defp keep_packing(items, weight_limit) do
+    {biggest_item, items_remaining} =
+      items
+      |> Enum.filter(&(&1.weight <= weight_limit))
+      |> pop_biggest_profit()
+
+    weight_remaining =
+      unless is_nil(biggest_item) do
+        weight_limit - biggest_item.weight
+      end
+
+    if weight_remaining do
+      [biggest_item | keep_packing(items_remaining, weight_remaining)]
+    else
+      keep_packing(items_remaining, weight_limit)
+    end
+  end
+
+  defp pop_biggest_profit(items) when length(items) == 0 do
+    {nil, []}
+  end
+
+  defp pop_biggest_profit(items) do
+    biggest = Enum.max_by(items, & &1.profit)
+    rest = List.delete(items, biggest)
+    {biggest, rest}
   end
 end
--- test/knapsack_test.exs
+++ test/knapsack_test.exs
@@ -2,7 +2,31 @@ defmodule KnapsackTest do
   use ExUnit.Case
   doctest Knapsack
 
-  test "greets the world" do
-    assert Knapsack.hello() == :world
+  import Knapsack
+
+  describe "0/1 Knapsack Problem" do
+    test "W = 4, profit[] = {1, 2, 3}, weight[] = {4, 5, 1}" do
+      objects = [
+        %{profit: 1, weight: 4},
+        %{profit: 2, weight: 5},
+        %{profit: 3, weight: 1}
+      ]
+
+      weight_limit = 4
+
+      assert zero_one_knapsack(objects, weight_limit) == 3
+    end
+
+    test "W = 3, profit[] = {1, 2, 3}, weight[] = {4, 5, 6}" do
+      objects = [
+        %{profit: 1, weight: 4},
+        %{profit: 2, weight: 5},
+        %{profit: 3, weight: 6}
+      ]
+
+      weight_limit = 3
+
+      assert zero_one_knapsack(objects, weight_limit) == 0
+    end
   end
 end
